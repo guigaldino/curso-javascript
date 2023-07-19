75a9079d83f50715f750b7906b17085dde9a8614
guigaldino <128167519+guigaldino@users.noreply.github.com>
2023-07-19 17:54:28 -0300

    aula 11 feita

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,6 @@
-console.log("O console funcionou corretamente.")
+var vel = 90
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){
+    console.log("Você ultrapassou a velocidade permitida. MULTADO!")
+}
+console.log("Dirija sempre usando cinto de segurança.")
