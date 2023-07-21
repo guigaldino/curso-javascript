04f84b5f1a836f29138dcbaf3649da64470f3a90
guigaldino <128167519+guigaldino@users.noreply.github.com>
2023-07-21 17:24:34 -0300

    aula 14 feita e exercicio iniciado

---
+++ aula14ex/ex16/script.js
@@ -0,0 +1,8 @@
+function contar(){
+    var inicio = document.getElementById('txtinicio')
+    var fim = document.getElementById('txtfim')
+    var passo = document.getElementById('txtpasso')
+    var input = document.getElementsByName('input')
+
+
+}
