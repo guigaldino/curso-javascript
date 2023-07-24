19d5416159f39d0ea4c618ee529ea25551214977
guigaldino <128167519+guigaldino@users.noreply.github.com>
2023-07-24 14:19:16 -0300

    ex16 feito e refeito

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -4,35 +4,30 @@ function contar() {
     let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
 
-    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        alert('[ERRO] Faltam dados!')
+    if (inicio.value.length === 0 || fim.value.length === 0 || passo.value.length === 0) {
+        alert("[ERRO] Preencha todos os campos em tente novamente!")
+        res.innerHTML = 'Insira os dados acima e aguarde o resultado...'
     } else {
-
-        res.innerHTML = "Contando: <br>"
+        res.innerHTML = 'Contando: <br>'
         let i = Number(inicio.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
 
-        if(p <= 0){
-            alert("Passo inválido! Considerando PASSO igual a 1")
+        if (p <= 0) {
+            alert('PASSO INVÁLIDO! PASSO séra definido como 1.')
             p = 1
         }
 
         if (i < f) {
-            //Contagem crescente
             for (let c = i; c <= f; c += p) {
-                res.innerHTML += ` ${c} \u{1f449}`
-            }
-        } else {
-            //contagem regressiva
-            for (let c = i; c >= f; c -= p) {
                 res.innerHTML += ` ${c} \u{1f449} `
             }
-
+        } else {
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
         }
-
-        res.innerHTML += `\u{1F3c1}`
-
+        res.innerHTML += `\u{1f3c1}`
     }
 
 }
