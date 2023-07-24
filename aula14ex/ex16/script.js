a71ffd4b5b79211b283f1a298ac921baba8893e0
guigaldino <128167519+guigaldino@users.noreply.github.com>
2023-07-24 11:44:57 -0300

    ex16 feito

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -1,8 +1,38 @@
-function contar(){
-    var inicio = document.getElementById('txtinicio')
-    var fim = document.getElementById('txtfim')
-    var passo = document.getElementById('txtpasso')
-    var input = document.getElementsByName('input')
+function contar() {
+    let inicio = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('[ERRO] Faltam dados!')
+    } else {
+
+        res.innerHTML = "Contando: <br>"
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if(p <= 0){
+            alert("Passo inválido! Considerando PASSO igual a 1")
+            p = 1
+        }
+
+        if (i < f) {
+            //Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+        } else {
+            //contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1f449} `
+            }
+
+        }
+
+        res.innerHTML += `\u{1F3c1}`
+
+    }
 
 }
