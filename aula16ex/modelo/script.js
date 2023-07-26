53cefd732473fb0bd813a7ba0263d78d0524ec8b
guigaldino <128167519+guigaldino@users.noreply.github.com>
2023-07-26 16:08:34 -0300

    exercicio em produção

--- aula16ex/modelo/script.js
+++ aula16ex/modelo/script.js
@@ -1,17 +1,26 @@
 function adicionar() {
     let numero = document.getElementById('txtnum')
-    let tabela = document.getElementById('seltab')
     let n = Number(numero.value)
+    let tabela = document.getElementById('seltab')
+    let res = document.getElementById('res')
+    let valores = []
 
     if (numero.value.length === 0) {
         numero.style.borderColor = 'red'
         alert('Por favor, digite um número.')
-    } else if (n < 0 || n > 100) {
+    } else if (n < 1 || n > 100) {
         numero.style.borderColor = 'red'
-        alert('Por favor, digite um número válido (entre 0 e 100)')
+        alert('Digite um número válido (entre 1 e 100)')
     } else {
-        numero.style.borderColor = 'normal'
-        let arr = [n]
-    }
-
+        for(let i = 0; i < valores.length; i++){
+            valores.push(n)
+            if(valores.indexOf(n) != -1){
+                let item = document.createElement('option')
+                item.text = `Valor ${valores} adicionado.`
+                tabela.appendChild(item)
+            } else { 
+                alert('Número já adicionado')
+            }
+        }
+    } 
 }
