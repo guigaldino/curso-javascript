e81c03867ab733377fafd91caaad49da6c747eea
guigaldino <128167519+guigaldino@users.noreply.github.com>
2023-07-27 18:48:20 -0300

    exercício realizado

--- aula16ex/modelo/script.js
+++ aula16ex/modelo/script.js
@@ -1,26 +1,54 @@
-function adicionar() {
-    let numero = document.getElementById('txtnum')
-    let n = Number(numero.value)
-    let tabela = document.getElementById('seltab')
-    let res = document.getElementById('res')
-    let valores = []
+let numeroInput = document.getElementById('txtnum')
+let tabela = document.getElementById('tabelaNumeros')
+let resultado = document.getElementById('resultado')
+let valores = []
 
-    if (numero.value.length === 0) {
-        numero.style.borderColor = 'red'
-        alert('Por favor, digite um número.')
-    } else if (n < 1 || n > 100) {
-        numero.style.borderColor = 'red'
-        alert('Digite um número válido (entre 1 e 100)')
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
     } else {
-        for(let i = 0; i < valores.length; i++){
-            valores.push(n)
-            if(valores.indexOf(n) != -1){
-                let item = document.createElement('option')
-                item.text = `Valor ${valores} adicionado.`
-                tabela.appendChild(item)
-            } else { 
-                alert('Número já adicionado')
-            }
-        }
-    } 
+        return false
+    }
+}
+
+function inLista(n, array) {
+    if (array.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+function adicionar() {
+    if (isNumero(numeroInput.value) && !inLista(numeroInput.value, valores)) {
+        valores.push(Number(numeroInput.value))
+        let itemTabela = document.createElement('option')
+        itemTabela.text = `Valor ${numeroInput.value} adicionado.`
+        tabela.appendChild(itemTabela)
+        resultado.innerHTML = ''
+    } else {
+        alert('Valor inválido ou já encontrado na lista')
+    }
+
+    numeroInput.value = ''
+    numeroInput.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar')
+    } else {
+        let total = valores.length
+        let maiorNumero = Math.max.apply(null, valores)
+        let menorNumero = Math.min.apply(null, valores)
+        let soma = valores.reduce((accumulator, value) => accumulator + value)
+        let media = soma / valores.length
+        resultado.innerHTML = ''
+        resultado.innerHTML += `<p> Ao todo foram cadastrados ${total} números</p>`
+        resultado.innerHTML += `<p> O maior valor cadastrado foi ${maiorNumero}</p>`
+        resultado.innerHTML += `<p> O menor valor cadastrado foi ${menorNumero}</p>`
+        resultado.innerHTML += `<p> A soma de todos os números cadastrados é igual a: ${soma}</p>`
+        resultado.innerHTML += `<p> A média de todos os números cadastrados é igual a: ${media}</p> `
+    }
 }
